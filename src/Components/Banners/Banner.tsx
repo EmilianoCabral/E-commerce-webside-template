907b0065aba883a05c5eb6ab6f46a39bf96f89cb
Emiliano cabral <emicabral95@gmail.com>
2024-05-21 23:46:41 -0300

    adding fonts and aesthetic elements

--- src/Components/Banners/Banner.tsx
+++ src/Components/Banners/Banner.tsx
@@ -7,12 +7,12 @@ export const Banner = () => {
         <section className="banner_container">
             <article className="background_banner">
                 <div className="section_title">
-                    <h1>Iphone 14 pro</h1>
+                    <h1>Iphone 14 <strong>pro</strong></h1>
                     <p>Created to change everything for the better. For everyone</p>
                     <Buttons title={"Shop now"} />
                 </div>
                 <div className="section_banner">
-                    <img className="banner_img" src="./src/assets/Banner-iphone-1.png" alt="iphone 14 pro" />
+                    <img className="banner_img" src="./src/assets/iPhone-14-Pro-1.png" alt="iphone 14 pro" />
                 </div>
             </article>
         </section>
